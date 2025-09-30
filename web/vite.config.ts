a6807caadc8be7190f95efc9e727e3cef8ff9908
BuffsOverNexus <buffsovernexus2@gmail.com>
2025-09-30 12:51:07 -0400

    Fixed tailwind compatibility and primereact

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './',
+  plugins: [react()],
+  base: "./",
   build: {
-    outDir: '../dist/web',
+    outDir: "../dist/web",
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
       },
     },
   },
